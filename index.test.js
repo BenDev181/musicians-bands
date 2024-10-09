58e736e0369e6a6e62e34d2f92bd0d36d9101ec2
BenDev181 <benjiro181@gmail.com>
2024-10-09 14:16:18 +0100

    All tests working

--- index.test.js
+++ index.test.js
@@ -1,3 +1,4 @@
+const { create } = require('../../sequelizeSetup/src/models');
 const { sequelize } = require('./db');
 const { Band, Musician, Song } = require('./index')
 
@@ -13,32 +14,36 @@ describe('Band, Musician, and Song Models', () => {
     })
 
     test('can create a Band', async () => {
-        // TODO - test creating a band
-        expect('NO TEST').toBe('EXPECTED VALUE HERE');
+        const createdBand = await Band.create({name: "Arctic Monkeys", genre: "Indie"})
+        expect(createdBand.name).toBe("Arctic Monkeys");
     })
 
     test('can create a Musician', async () => {
-        // TODO - test creating a musician
-        expect('NO TEST').toBe('EXPECTED VALUE HERE');
+        const createdMusician = await Musician.create({name: "Taylor Swift", genre: "Pop"})
+        expect(createdMusician.name).toBe("Taylor Swift");
     })
 
     test('can update a Band', async () => {
-        // TODO - test updating a band
-        expect('NO TEST').toBe('EXPECTED VALUE HERE');
+        const createdBand = await Band.create({name: "Arctic Monkeys", genre: "Indie"})
+        const updatedBand = await createdBand.update({name: "The Beatles"})
+        expect(updatedBand.name).toBe("The Beatles");
     })
 
     test('can update a Musician', async () => {
-        // TODO - test updating a musician
-        expect('NO TEST').toBe('EXPECTED VALUE HERE');
+        const createdMusician = await Musician.create({name: "Taylor Swift", genre: "Pop"})
+        const updatedMusician = await createdMusician.update({name: "Post Malone"})
+        expect(updatedMusician.name).toBe("Post Malone");
     })
 
     test('can delete a Band', async () => {
-        // TODO - test deleting a band
-        expect('NO TEST').toBe('EXPECTED VALUE HERE');
+        const createdBand = await Band.create({name: "Arctic Monkeys", genre: "Indie"})
+        const deletedBand = await createdBand.destroy()
+        expect(deletedBand.name).toBe("Arctic Monkeys");
     })
 
     test('can delete a Musician', async () => {
-        // TODO - test deleting a musician
-        expect('NO TEST').toBe('EXPECTED VALUE HERE');
+        const createdMusician = await Musician.create({name: "Taylor Swift", genre: "Pop"})
+        const deletedMusician = await createdMusician.destroy()
+        expect(deletedMusician.name).toBe("Taylor Swift");
     })
 })
